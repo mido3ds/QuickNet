6beaa02dafaca87d7340db1173f077659b9ab0d3
Mahmoud Adas <mido3ds@gmail.com>
2017-08-10 16:06:09 +0200

    added GMTDateTime and tested

---
+++ src/utility/Date.h
@@ -0,0 +1,23 @@
+#ifndef __Date_h__
+#define __Date_h__
+
+namespace date
+{
+#include <ctime>
+
+    inline char* GMTDateTime()
+    {
+        time_t rawTime;
+        tm* ptm;
+        char* result;
+
+        time(&rawTime);
+        ptm = gmtime(&rawTime);
+
+        result = asctime(ptm);
+        result[strlen(result)-1] = '\0';
+        return result;
+    }
+}
+
+#endif  /* __Date_h__ */
