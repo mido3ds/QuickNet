1bbb52b7fc8622e93d80a6d543c643ad6c647265
Mahmoud Adas <mido3ds@gmail.com>
2017-10-04 08:19:55 +0200

    built TimeOutTest but not running yet

--- test/Socket.cpp
+++ test/Socket.cpp
@@ -2,21 +2,30 @@
 using namespace cpunit;
 
 #include <string>
+#include <thread>
 using namespace std;
 #include <Socket.h>
 
 namespace TimeOutTest 
 {
     CPUNIT_TEST(TimeOutTest, test_1_second) {
-        string toSend = "some string for testing";
+        string toSend = "some string for testing", whatIReceived;
 
         Socket receiver("1000", TCP, toBind);
         receiver.Listen(1);
-        
         Socket sender("1000", TCP, toConnect);
-        receiver.Accept();
-        sender.Send(toSend);
+        
+        try {
+            thread([&]{
+                whatIReceived = receiver.Accept().Receive(1);
+            });
+            thread([&]{
+                sender.Send(toSend);
+            }).join();
+        } catch(...) {
+            assert_true("caught exception", false);
+        }
 
-        assert_equals(toSend, receiver.Receive(1));
+        assert_equals(toSend, whatIReceived);
     }
 }
